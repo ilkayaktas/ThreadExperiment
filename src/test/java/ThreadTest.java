1d3ffb4fa78fcd2d8db1c0de00634d2431b9911a
İlkay AKTAŞ <iaktas@aselsan.com.tr>
2021-06-28 10:23:20 +0300

    Thread pool ve singeleThreadExecutor methods are added.

--- src/test/java/ThreadTest.java
+++ src/test/java/ThreadTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by ilkayaktas on 28.06.2021 at 09:16.
@@ -9,10 +10,15 @@ import java.util.concurrent.*;
 class ThreadTest {
     Long startTime;
 
+    private void log(String str){
+        System.out.println((System.currentTimeMillis()-startTime)+" ms" +"\t"+str);
+    }
+
     @BeforeEach
     public void setUp(){
         startTime = System.currentTimeMillis();
     }
+
     @Test
     public void testExecutor(){
         Executor executor = Executors.newSingleThreadExecutor();
@@ -84,8 +90,153 @@ class ThreadTest {
         }
     }
 
-    private void log(String str){
-        System.out.println((System.currentTimeMillis()-startTime)+" ms" +"\t"+str);
+    @Test
+    public void testFixedSizeThreadPool() throws ExecutionException, InterruptedException {
+
+        log("Test started.");
+
+        ThreadPoolExecutor executor =
+                (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
+
+        Future f1 = executor.submit(() -> {
+            Thread.sleep(1000);
+            Long time = System.currentTimeMillis() - startTime;
+            log("Thread 1 finished at " + time+ "\t"+"Pool Size:" + executor.getPoolSize() +"\t\tQueue:"+executor.getQueue().size()+"\t\tCore Pool:"+executor.getCorePoolSize()+"\t\tActive Count:"+executor.getActiveCount()+"\t\tCompleted:"+executor.getCompletedTaskCount());
+            return String.valueOf(time);
+        });
+
+        Future f2 = executor.submit(() -> {
+            Thread.sleep(2000);
+            Long time = System.currentTimeMillis() - startTime;
+            log("Thread 2 finished at " + time+ "\t"+"Pool Size:" + executor.getPoolSize() +"\t\tQueue:"+executor.getQueue().size()+"\t\tCore Pool:"+executor.getCorePoolSize()+"\t\tActive Count:"+executor.getActiveCount()+"\t\tCompleted:"+executor.getCompletedTaskCount());
+            return String.valueOf(time);
+        });
+
+        Future f3 = executor.submit(() -> {
+            Thread.sleep(3000);
+            Long time = System.currentTimeMillis() - startTime;
+            log("Thread 3 finished at " + time+ "\t"+"Pool Size:" + executor.getPoolSize() +"\t\tQueue:"+executor.getQueue().size()+"\t\tCore Pool:"+executor.getCorePoolSize()+"\t\tActive Count:"+executor.getActiveCount()+"\t\tCompleted:"+executor.getCompletedTaskCount());
+            return String.valueOf(time);
+        });
+
+        Future f4 = executor.submit(() -> {
+            Thread.sleep(1500);
+            Long time = System.currentTimeMillis() - startTime;
+            log("Thread 4 finished at " + time+ "\t"+"Pool Size:" + executor.getPoolSize() +"\t\tQueue:"+executor.getQueue().size()+"\t\tCore Pool:"+executor.getCorePoolSize()+"\t\tActive Count:"+executor.getActiveCount()+"\t\tCompleted:"+executor.getCompletedTaskCount());
+            return String.valueOf(time);
+        });
+
+        f1.get();
+        f2.get();
+        f3.get();
+        f4.get();
+
+        log("Test finished.");
+
     }
 
+    @Test
+    public void testCachedThreadPool() throws ExecutionException, InterruptedException {
+
+        log("Test started.");
+
+        /*
+         İhtiyaç halinde thread cache büyür
+         Herşey SynchronousQueue'ya atılır. Burada insert ve remove her zaman aynı anda yapıldığı için queue sıfır görünür.
+         */
+        ThreadPoolExecutor executor =
+                (ThreadPoolExecutor) Executors.newCachedThreadPool();
+
+        Future f1 = executor.submit(() -> {
+            Thread.sleep(1000);
+            Long time = System.currentTimeMillis() - startTime;
+            log("Thread 1 finished at " + time+ "\t"+"Pool Size:" + executor.getPoolSize() +"\t\tQueue:"+executor.getQueue().size()+"\t\tCore Pool:"+executor.getCorePoolSize()+"\t\tActive Count:"+executor.getActiveCount()+"\t\tCompleted:"+executor.getCompletedTaskCount());
+            return String.valueOf(time);
+        });
+
+        Future f2 = executor.submit(() -> {
+            Thread.sleep(2000);
+            Long time = System.currentTimeMillis() - startTime;
+            log("Thread 2 finished at " + time+ "\t"+"Pool Size:" + executor.getPoolSize() +"\t\tQueue:"+executor.getQueue().size()+"\t\tCore Pool:"+executor.getCorePoolSize()+"\t\tActive Count:"+executor.getActiveCount()+"\t\tCompleted:"+executor.getCompletedTaskCount());
+            return String.valueOf(time);
+        });
+
+        Future f3 = executor.submit(() -> {
+            Thread.sleep(3000);
+            Long time = System.currentTimeMillis() - startTime;
+            log("Thread 3 finished at " + time+ "\t"+"Pool Size:" + executor.getPoolSize() +"\t\tQueue:"+executor.getQueue().size()+"\t\tCore Pool:"+executor.getCorePoolSize()+"\t\tActive Count:"+executor.getActiveCount()+"\t\tCompleted:"+executor.getCompletedTaskCount());
+            return String.valueOf(time);
+        });
+
+        Future f4 = executor.submit(() -> {
+            Thread.sleep(1500);
+            Long time = System.currentTimeMillis() - startTime;
+            log("Thread 4 finished at " + time+ "\t"+"Pool Size:" + executor.getPoolSize() +"\t\tQueue:"+executor.getQueue().size()+"\t\tCore Pool:"+executor.getCorePoolSize()+"\t\tActive Count:"+executor.getActiveCount()+"\t\tCompleted:"+executor.getCompletedTaskCount());
+            return String.valueOf(time);
+        });
+
+        f1.get();
+        f2.get();
+        f3.get();
+        f4.get();
+
+        log("Test finished.");
+
+    }
+
+    @Test
+    public void testSingleThreadExecutor() throws ExecutionException, InterruptedException {
+        log("Test started.");
+
+        AtomicInteger counter = new AtomicInteger();
+
+        /*
+         Threadler eklendiği sıra ile çalıştırılır. Aynı anda sadece tek thread çalıştırılır.
+         */
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future f1 = executor.submit(() -> {
+            Long time = System.currentTimeMillis() - startTime;
+            log("1.Executed at "+time);
+            counter.set(1);
+        });
+        Future f2 = executor.submit(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Long time = System.currentTimeMillis() - startTime;
+            log("2.Executed at "+time);
+
+            counter.compareAndSet(1, 2);
+        });
+        Future f3 = executor.submit(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Long time = System.currentTimeMillis() - startTime;
+            log("3.Executed at "+time);
+
+            counter.compareAndSet(2, 3);
+        });
+        Future f4 = executor.submit(() -> {
+            Long time = System.currentTimeMillis() - startTime;
+            log("4.Executed at "+time);
+
+            counter.compareAndSet(3, 4);
+        });
+
+        // Random get() call
+        f4.get();
+        f1.get();
+        f3.get();
+        f2.get();
+
+        log("Result:" + counter.toString());
+        log("Test finished.");
+
+    }
 }
